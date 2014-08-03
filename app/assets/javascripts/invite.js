2aeae935e121e495c6ed6412a4f41ac2ff4e94a6
Bryan Collazo <bcollazo@mit.edu>
2014-08-03 09:36:55 -0400

    match

--- app/assets/javascripts/invite.js
+++ app/assets/javascripts/invite.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
 	$(".user-row").click(function() {
 		$(this).addClass("active");
 	});
+
+	$("#invite_all_btn").click(function() {
+		location.href = "/matched";
+	});
+
+	$("#invite_some_btn").click(function() {
+		location.href = "/matched";
+	});
+
+
 });
