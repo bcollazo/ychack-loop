e8a1aac379c9abd40f0972f30d0adcbe03a7387f
Graham Turk <grahamturk@Grahams-MacBook-Air.local>
2014-08-03 01:17:08 -0700

    new addition

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,12 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
+ 
+   var $target = $( event.currentTarget );
+ 
+   $target.closest( '.btn-group' )
+      .find( '[data-bind="label"]' ).text( $target.text() )
+         .end()
+      .children( '.dropdown-toggle' ).dropdown( 'toggle' );
+ 
+   return false;
+ 
+});
--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,30 @@
+$( document.body ).on( 'click', '.btn-input li', function( event ) {
+ 
+   var $target = $( event.currentTarget );
+ 
+   $target.closest( 'li' )
+      .find( '[data-bind="label"]' ).text( $target.text() )
+         .end()
+      .children( '.dropdown-toggle' ).dropdown( 'toggle' );
+ 
+   return false;
+ 
+});
+
 var main = function() {
-	$("#live").mousehover(function () {
-		$(this).toggleClass("activate");
+	$(".btn-input .dropdown-menu li a").hover(function {
+		$(this).toggleClass("hover-select");
+	});
+
+	$(".modal a").click(function() {
+		console.log("adfs");
+		var post = $(this).val();
+		var new_line = $("<li>").text(post);
+		new_line.prependTo(".list-unstyled");
+	)};
+
+	$(".nav.navbar-nav > li").hover(function {
+		$(this).toggle("active");
 	});
 };
 
